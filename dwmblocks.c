5ecb29e08caa55f8dd8627c6137bb033b5c5556e
Tom Böhmer <qtom.boehmer@googlemail.com>
2020-03-23 16:36:58 +0100

    use size-aware buffers instead of cstrings for statusbar and statusstr; write delimiter in getstatus instead of in getcmd

--- dwmblocks.c
+++ dwmblocks.c
@@ -5,7 +5,10 @@
 #include<signal.h>
 #include<X11/Xlib.h>
 #define LENGTH(X)               (sizeof(X) / sizeof (X[0]))
+#define STRLEN(X) (sizeof(X) - 1)
 #define CMDLENGTH		50
+#define STATUSLENGTH 256
+#define MIN(X, Y) ({ typeof(X) __x = (X); typeof(Y) __y = (Y); __x > __y ? __y : __x; })
 
 typedef struct {
 	char* icon;
@@ -20,36 +23,51 @@ static void getsigcmds(int signal);
 static void setupsignals();
 static void sighandler(int signum);
 #endif
-static int getstatus(char *str, char *last);
+static int getstatus();
 static void setroot();
 static void statusloop();
 static void termhandler(int signum);
 
-
 #include "blocks.h"
+#include "buffer.h"
+
+typedef BUFFER(CMDLENGTH) CommandBuffer;
+typedef BUFFER(STATUSLENGTH) StatusBuffer;
 
 static Display *dpy;
 static int screen;
 static Window root;
-static char statusbar[LENGTH(blocks)][CMDLENGTH] = {0};
-static char statusstr[2][256];
+static CommandBuffer statusbar[LENGTH(blocks)];
+static StatusBuffer statusstr;
 static int statusContinue = 1;
 static void (*writestatus) () = setroot;
 
-//opens process *cmd and stores output in *output
-void getcmd(const Block *block, char *output)
+static char* str_copy(char* dest, char const* src)
 {
-	strcpy(output, block->icon);
-	char *cmd = block->command;
-	FILE *cmdf = popen(cmd,"r");
+	while(*src != '\0')
+	{
+		*dest = *src;
+		++dest;
+		++src;
+	}
+	return dest;
+}
+
+//opens process *block->command and stores output in *output
+void getcmd(const Block *block, CommandBuffer* output)
+{
+	output->count = str_copy(output->data, block->icon) - output->data;
+
+	FILE *cmdf = popen(block->command, "r");
+
 	if (!cmdf)
-		return;
-	int i = strlen(block->icon);
-	fgets(output+i, CMDLENGTH-i, cmdf);
-	i = strlen(output);
-	if (delim != '\0' && --i)
-		output[i++] = delim;
-	output[i++] = '\0';
+	{ return; }
+
+	output->count += fread(output->data + output->count, 1, LENGTH(output->data) - output->count, cmdf);
+
+	if(output->data[output->count - 1] == '\n')
+	{ --output->count; }
+
 	pclose(cmdf);
 }
 
@@ -60,7 +78,7 @@ void getcmds(int time)
 	{	
 		current = blocks + i;
 		if ((current->interval != 0 && time % current->interval == 0) || time == -1)
-			getcmd(current,statusbar[i]);
+		{ getcmd(current, statusbar + i); }
 	}
 }
 
@@ -72,7 +90,7 @@ void getsigcmds(int signal)
 	{
 		current = blocks + i;
 		if (current->signal == signal)
-			getcmd(current,statusbar[i]);
+			getcmd(current, statusbar + i);
 	}
 }
 
@@ -87,19 +105,37 @@ void setupsignals()
 }
 #endif
 
-int getstatus(char *str, char *last)
+int getstatus()
 {
-	strcpy(last, str);
-	str[0] = '\0';
-	for(size_t i = 0; i < LENGTH(blocks); i++)
-		strcat(str, statusbar[i]);
-	str[strlen(str)-1] = '\0';
-	return strcmp(str, last);//0 if they are the same
+	StatusBuffer old;
+	buffer_copy(&old, &statusstr);
+
+	buffer_copy(&statusstr, &statusbar[0]);
+
+	for(size_t i = 1; i < LENGTH(blocks); ++i)
+	{
+		if(statusstr.count + statusbar[i].count + 1 > STRLEN(statusstr.data))
+		{
+			char error[] = "error: status string is too long to be stored in the buffer";
+			memcpy(statusstr.data, error, MIN(STRLEN(statusstr.data), STRLEN(error)));
+			statusstr.count = MIN(STRLEN(statusstr.data), STRLEN(error));
+			break;
+		}
+
+		if(delim != '\0' && statusbar[i].count > 0)
+		{ statusstr.data[statusstr.count++] = delim; }
+
+		buffer_append(&statusstr, &statusbar[i]);
+	}
+
+	statusstr.data[statusstr.count++] = '\0';
+
+	return buffer_eq(&statusstr, &old);
 }
 
 void setroot()
 {
-	if (!getstatus(statusstr[0], statusstr[1]))//Only set root if text has changed.
+	if (getstatus())//Only set root if text has changed.
 		return;
 	Display *d = XOpenDisplay(NULL);
 	if (d) {
@@ -107,15 +143,15 @@ void setroot()
 	}
 	screen = DefaultScreen(dpy);
 	root = RootWindow(dpy, screen);
-	XStoreName(dpy, root, statusstr[0]);
+	XStoreName(dpy, root, statusstr.data);
 	XCloseDisplay(dpy);
 }
 
 void pstdout()
 {
-	if (!getstatus(statusstr[0], statusstr[1]))//Only write out if text has changed.
+	if (getstatus())//Only write out if text has changed.
 		return;
-	printf("%s\n",statusstr[0]);
+	printf("%s\n", statusstr.data);
 	fflush(stdout);
 }
 

68256f7aa338afaf44449b345c46db61eed8510e
Jiří Szkandera <jirik.sz@gmail.com>
2020-03-23 16:36:00 +0100

    prepare for pull request v2

--- blocks.h
+++ blocks.h
@@ -1,6 +1,6 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
 
 	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},

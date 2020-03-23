4fd6013463b098747357bb4a09ac0175add9c777
Jiří Szkandera <jirik.sz@gmail.com>
2020-03-23 18:22:22 +0100

    add corona script

--- blocks.h
+++ blocks.h
@@ -1,12 +1,13 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/* Icon */	/* Command */				/* Update Interval */	/* Update Signal */
-	{" ", "~/.local/bin/desktop/statusbar/memory",		10,		0},
-	{"", "~/.local/bin/desktop/statusbar/cputemp",		10,		0},
-	{"", "~/.local/bin/desktop/statusbar/battery",		30,		3},
-	{"", "~/.local/bin/desktop/statusbar/volume",		30,		2},
-	{"", "~/.local/bin/desktop/statusbar/layout",		30,		1},
-	{" ", "~/.local/bin/desktop/statusbar/clock",		5,		0},
+	/* Icon */        /* Command */           /* Update Interval */   /* Update Signal */
+	{"",   "~/.local/bin/desktop/statusbar/corona",       7200,         0},
+	{" ", "~/.local/bin/desktop/statusbar/memory",       10,         0},
+	{"",   "~/.local/bin/desktop/statusbar/cputemp",      10,         0},
+	{"",   "~/.local/bin/desktop/statusbar/battery",      30,         0},
+	{"",   "~/.local/bin/desktop/statusbar/volume",       30,         1},
+	{"",   "~/.local/bin/desktop/statusbar/layout",       30,         2},
+	{" ", "~/.local/bin/desktop/statusbar/clock",         5,         0},
 };
 
 // sets delimeter between status commands. NULL pointer means no delimeter.

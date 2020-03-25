159f33e21620fb99d69edd6db48035716075944f
Jiří Szkandera <jirik.sz@gmail.com>
2020-03-25 20:26:36 +0100

    add corona

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/* Icon */        /* Command */           /* Update Interval */   /* Update Signal */
-	{"",   "~/.local/bin/desktop/statusbar/corona",       7200,         0},
+	{" ",   "~/.local/bin/desktop/statusbar/corona",      1800,       0},
 	{" ", "~/.local/bin/desktop/statusbar/memory",       10,         0},
 	{"",   "~/.local/bin/desktop/statusbar/cputemp",      10,         0},
 	{"",   "~/.local/bin/desktop/statusbar/battery",      30,         0},

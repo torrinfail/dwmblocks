1d6e863459532ef933cfcfd297022c9f39106e58
Tom Böhmer <qtom.boehmer@googlemail.com>
2020-05-28 14:30:04 +0200

    update blocks.def.h to match dwmblocks.c

--- blocks.def.h
+++ blocks.def.h
@@ -1,20 +1,13 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
-	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "~/bin/statusbar/volume",						0,		10},
-
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"", "~/bin/statusbar/battery",						5,		0},
-
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
-
-	{"", "~/bin/statusbar/clock",						5,		0},
+	/*Icon*/        /*Command*/        /*Update Interval*/        /*Update Signal*/
+	{ "",     "netif.ip",              60,                        0 },
+	{ "",     "volume",                0,                         1 },
+	{ "",     "datetime",              5,                         0 },
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+// Sets delimeter between status commands.
+static char delim[]       = " ][ ";
+static char left_delim[]  = "[ ";
+static char right_delim[] = " ]";

13c7700f34b74c4cf91dfb261c61ca1ca1eac6c3
aidan <aidan@Edward.localdomain>
2020-08-29 11:58:28 -0600

    Fixed issue with empty command delimiter This meant that the delimiters were added when when both the icon and output where empty.

--- dwmblocks.c
+++ dwmblocks.c
@@ -56,6 +56,8 @@ void getcmd(const Block *block, char *output)
 	int i = strlen(block->icon);
 	fgets(output+i, CMDLENGTH-i-delimLen, cmdf);
 	i = strlen(output);
+	if (i == 0)//return if block and command output are both empty
+		return;
 	if (delim[0] != '\0') {
 		//only chop off newline if one is present at the end
 		i = output[i-1] == '\n' ? i-1 : i;

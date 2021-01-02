9485fa2c0cc30c49138ac298a7758619fe2b59c0
Felipe Marcon <felipemarconwd@gmail.com>
2021-01-02 00:13:49 -0300

    Add options to pad left and right side statusbar

--- blocks.def.h
+++ blocks.def.h
@@ -9,3 +9,7 @@ static const Block blocks[] = {
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
+
+// Sets delimiters around the full statusbar. NULL character ('\0') means no delimeter.
+static char leftpad[]  = "   ";
+static char rightpad[] = " ";

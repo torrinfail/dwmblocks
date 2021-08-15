8fa01e3905e22bd3ea6db57dfbc4c849c373e54d
Emilia Trollros <et-8@emiliatrollros.se>
2021-08-15 22:30:08 +0200

    Making the code for getting the memory values multilingual

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"Mem:", "free -h | awk 'NR==2 { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };

406e4a4d1207055f8abbc055abfac191abc32e22
tinnamchoi <tinnam.choi@gmail.com>
2023-08-02 20:31:25 +0930

    delimeter -> delimiter

--- blocks.def.h
+++ blocks.def.h
@@ -6,6 +6,6 @@ static const Block blocks[] = {
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
